2bf0219490ee7f5d57a3913b61f50b8c82228900
Mike MacDonald <crazymykl@gmail.com>
2016-08-07 12:08:11 -0400

    Crash into me

--- src/entity.rs
+++ src/entity.rs
@@ -1,62 +1,97 @@
-use {Point, MU, WIDTH, HEIGHT};
+use MU;
+use std::cell::{Cell, RefCell};
+use std::rc::Rc;
+use na::{Point2, Vector2, Isometry2, Translation};
+use na;
+use ncollide::world::{GeometricQueryType, CollisionGroups, CollisionWorld};
+use ncollide::shape::{Cuboid, ShapeHandle2};
+
+pub type CollideWorld = CollisionWorld<Point2<f32>, Isometry2<f32>, Entity>;
 
 #[derive(Clone)]
 pub struct Entity {
-    pos: Point,
-    pub color: [f32; 4],
-    dv: Point,
-    width: f64,
-    height: f64,
+    color: [f32; 4],
+    velocity: Option<Rc<Cell<Vector2<f32>>>>,
+    width: f32,
+    height: f32,
+    world: Rc<RefCell<CollideWorld>>,
+    index: usize,
 }
 
+
 impl Entity {
-    pub fn new(pos: Point, color: [f32; 4], w: f64, h: f64) -> Self {
-        Entity {
-            pos: pos,
+    pub fn new(pos: Point2<f32>, color: [f32; 4],
+        w: f32, h: f32, velocity: Option<Vector2<f32>>,
+        world: Rc<RefCell<CollideWorld>>,
+        idx: usize) -> Self {
+
+        let entity = Entity {
             color: color,
-            dv: Point(0.0, 0.0),
+            velocity: velocity.map(|v| Rc::new(Cell::new(v))),
             width: w,
             height: h,
-        }
+            world: world.clone(),
+            index: idx,
+        };
+
+        let rect = ShapeHandle2::new(Cuboid::new(Vector2::new(w / 2.0, h / 2.0)));
+
+        let pos = Isometry2::new(pos.to_vector(), na::zero());
+
+        let mut groups = CollisionGroups::new();
+        groups.set_membership(&[1]);
+
+        let mut world = world.borrow_mut();
+
+        world.add(idx, pos, rect, groups, GeometricQueryType::Contacts(0.0), entity.clone());
+        world.update();
+
+        entity
     }
 
     pub fn geometry(&self) -> [f64; 4] {
-        [self.pos.0, self.pos.1, self.width, self.height]
+        let (x, y) = self.position();
+
+        [x as f64, y as f64, self.width as f64, self.height as f64]
+    }
+
+    pub fn color(&self) -> [f32; 4] {
+        self.color
+    }
+
+    pub fn velocity(&self) -> &Option<Rc<Cell<Vector2<f32>>>> {
+        &self.velocity
     }
 
     pub fn nudge(&mut self) {
-        self.pos.0 += self.dv.0;
-        self.pos.1 += self.dv.1;
+        if let Some(ref delta_v) = self.velocity {
+            let dv = delta_v.get();
+            let mut world = self.world.borrow_mut();
+            let pos = world.collision_object(self.index).unwrap().position;
+            let new_pos = pos.append_translation(&dv);
 
-        self.dv.0 *= MU;
-        self.dv.1 *= MU;
-
-        self.clamp();
+            world.deferred_set_position(self.index, new_pos);
+            delta_v.set(dv * MU);
+        }
     }
 
-    pub fn adjust_dx(&mut self, dx: f64) {
-        self.dv.0 += dx;
+    pub fn adjust_dx(&mut self, dx: f32) {
+        if let Some(ref delta_v) = self.velocity {
+            delta_v.set(delta_v.get() + Vector2::new(dx, 0.0));
+        }
     }
 
-    pub fn adjust_dy(&mut self, dy: f64) {
-        self.dv.1 += dy;
+    pub fn adjust_dy(&mut self, dy: f32) {
+        if let Some(ref delta_v) = self.velocity {
+            delta_v.set(delta_v.get() + Vector2::new(0.0, dy));
+        }
     }
 
-    fn clamp(&mut self) {
-        let (w, h) = (self.max_width(), self.max_height());
+    fn position(&self) -> (f32, f32) {
+        let world = self.world.borrow();
+        let pos = world.collision_object(self.index).unwrap().position.translation;
 
-        if self.pos.0 < 0.0 { self.pos.0 = 0.0 };
-        if self.pos.0 > w   { self.pos.0 = w };
-        if self.pos.1 < 0.0 { self.pos.1 = 0.0 };
-        if self.pos.1 > h   { self.pos.1 = h };
-    }
-
-    fn max_height(&self) -> f64 {
-        HEIGHT as f64 - self.height
-    }
-
-    fn max_width(&self) -> f64 {
-        WIDTH as f64 - self.width
+        (pos.x - self.width / 2.0, pos.y - self.height / 2.0)
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 extern crate piston_window;
+extern crate ncollide;
+extern crate nalgebra as na;
 
 mod entity;
+mod velocity_bouncer;
 
+use std::rc::Rc;
+use std::cell::RefCell;
 use piston_window::*;
+use na::{Point2, Vector2};
+use ncollide::world::CollisionWorld;
 use entity::Entity;
+use velocity_bouncer::VelocityBouncer;
 
-#[derive(Debug, Clone)]
-pub struct Point(f64, f64);
-
-const MU: f64 = 0.93;
+const MU: f32 = 1.0;
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
 fn main() {
     let title = "Goober";
     let mut i = 0;
+    let world = Rc::new(RefCell::new(CollisionWorld::new(0.02, true)));
+    world.borrow_mut().register_contact_handler("VelocityBouncer", VelocityBouncer);
     let mut squares = [
-        Entity::new(Point(0.0, 0.0), [0.3, 0.0, 0.7, 0.5], 25.0, 25.0),
-        Entity::new(Point(100.0, 100.0), [0.7, 0.0, 0.3, 0.5], 25.0, 25.0),
+        Entity::new(Point2::new(100.0, 210.0), [0.3, 0.0, 0.7, 0.5], 50.0, 50.0, Some(Vector2::new(0.4, 0.25)), world.clone(), 1),
+        Entity::new(Point2::new(230.0, 100.0), [0.7, 0.0, 0.3, 0.5], 50.0, 50.0, None, world.clone(), 2),
+        Entity::new(Point2::new(150.0, 163.0), [0.7, 0.0, 0.3, 0.5], 25.0, 25.0, None, world.clone(), 3),
+        Entity::new(Point2::new(0.0, HEIGHT as f32 / 2.0), [0.7, 0.0, 0.3, 0.5], 10.0, HEIGHT as f32, None, world.clone(), 4),
+        Entity::new(Point2::new(WIDTH as f32, HEIGHT as f32 / 2.0), [0.7, 0.0, 0.3, 0.5], 10.0, HEIGHT as f32, None, world.clone(), 5),
+        Entity::new(Point2::new(WIDTH as f32 / 2.0, 0.0), [0.7, 0.0, 0.3, 0.5], WIDTH as f32, 10.0, None, world.clone(), 6),
+        Entity::new(Point2::new(WIDTH as f32 / 2.0, HEIGHT as f32), [0.7, 0.0, 0.3, 0.5], WIDTH as f32, 10.0, None, world.clone(), 7),
+        Entity::new(Point2::new(500.0, 310.0), [0.3, 0.0, 0.7, 0.5], 50.0, 50.0, Some(Vector2::new(0.4, 0.25)), world.clone(), 8),
     ];
 
     let mut window: PistonWindow = WindowSettings::new(title, [WIDTH, HEIGHT])
@@ -31,7 +44,7 @@ fn main() {
             window.draw_2d(&e, |c, g| {
                 clear([0.0, 0.0, 0.0, 1.0], g);
                 for sq in &squares {
-                    rectangle(sq.color, sq.geometry(), c.transform, g);
+                    rectangle(sq.color(), sq.geometry(), c.transform, g);
                 }
             });
         }
@@ -42,13 +55,19 @@ fn main() {
                 Button::Keyboard(Key::Down)  => squares[i].adjust_dy(1.0),
                 Button::Keyboard(Key::Left)  => squares[i].adjust_dx(-1.0),
                 Button::Keyboard(Key::Right) => squares[i].adjust_dx(1.0),
-                Button::Keyboard(Key::Space) => if i == squares.len() - 1 { i = 0 } else { i += 1 },
+                Button::Keyboard(Key::Space) => loop  {
+                    if i >= squares.len() - 1 { i = 0; } else { i += 1; }
+                    if squares[i].velocity().is_some() { break; }
+                },
                 _ => ()
             }
         }
 
         if let Some(_) = e.update_args() {
-            squares[i].nudge();
+            world.borrow_mut().update();
+            for sq in &mut squares {
+                sq.nudge();
+            }
         }
     }
 }
